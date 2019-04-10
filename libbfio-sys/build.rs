8e263641c820c5b59164bd9969c7a41e22294374
Ohad Ravid <ohad.rv@gmail.com>
2019-04-10 12:02:51 +0200

    Feature/improve build and add support for windows (#1, #2)
    * Initial working build for windows
    
    * Static link really works now
    
    * document `build_lib`  and make it more generic
    
    * Improve docs
    
    * Ignore vs2015 does not exist
    
    * PR fixes
    
    * Better error when python is not found vs error in python script
    
    * Extract common-build crate
    
    * Use common-build for libfsntfs
    
    * Add libfsntfs deps in windows, only rerun on src changes

--- libbfio-sys/build.rs
+++ libbfio-sys/build.rs
@@ -1,129 +1,54 @@
-extern crate bindgen;
-
 use failure::{bail, Error};
+use libyal_rs_common_build::{build_lib, generate_bindings};
 use std::env;
-use std::fs::File;
-use std::io;
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
 
-
-fn build_static() -> Option<PathBuf> {
-    let libbfio = if let Ok(local_install) = env::var("LIBBFIO_STATIC_LIBPATH") {
+fn build_and_link_static() -> PathBuf {
+    let libbfio = if let Ok(local_install) = env::var("LIBBFIO_LIBPATH") {
         PathBuf::from(local_install)
     } else {
         env::current_dir().unwrap().join("libbfio")
     };
 
-    let target = libbfio.join("dist");
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=static=libbfio");
 
-    println!("building with prefix={}", target.display());
-
-    Command::new("sh")
-        .arg("synclibs.sh")
-        .current_dir(&libbfio)
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .status()
-        .expect("synclibs failed");
-
-    Command::new("sh")
-        .arg("autogen.sh")
-        .current_dir(&libbfio)
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .status()
-        .expect("autogen failed");
-
-    Command::new("sh")
-        .arg("configure")
-        .arg("--enable-shared=no")
-        .arg(format!("--prefix={}", target.display()))
-        .current_dir(&libbfio)
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .status()
-        .expect("configure failed");
-
-    Command::new("make")
-        .current_dir(&libbfio)
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .status()
-        .expect("make failed");
-
-    Command::new("make")
-        .arg("install")
-        .current_dir(&libbfio)
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .status()
-        .expect("make install failed");
-
-    assert!(
-        target.join("lib").exists(),
-        "Expected {} to exist",
-        target.join("lib").display()
-    );
-
-    println!("cargo:rustc-link-lib=static=bfio");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        target.join("lib").canonicalize().unwrap().to_string_lossy()
-    );
-
-    Some(target)
-}
-
-fn link_dynamic() -> Option<PathBuf> {
-    println!("cargo:rustc-link-lib=dylib=bfio");
-
-    if let Ok(location) = env::var("LIBBFIO_DYNAMIC_LIBPATH") {
-        assert!(
-            PathBuf::from(&location).exists(),
-            "path passed in LIBBFIO_DYNAMIC_LIBPATH does not exist!"
-        );
-        println!("cargo:rustc-link-search=native={}", location);
-
-        return Some(location.into());
+        // Also static-link deps (otherwise we'll get missing symbols at link time).
+        println!("cargo:rustc-link-lib=static=libcerror");
+        println!("cargo:rustc-link-lib=static=libcdata");
+        println!("cargo:rustc-link-lib=static=libcthreads");
+    } else {
+        println!("cargo:rustc-link-lib=static=bfio");
     }
 
-    None
+    build_lib(libbfio, false)
+}
+
+fn build_and_link_dynamic() -> PathBuf {
+    let libbfio = if let Ok(local_install) = env::var("LIBBFIO_LIBPATH") {
+        PathBuf::from(local_install)
+    } else {
+        env::current_dir().unwrap().join("libbfio")
+    };
+
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=dylib=libbfio");
+    } else {
+        println!("cargo:rustc-link-lib=dylib=bfio");
+    }
+
+    build_lib(libbfio, true)
 }
 
 fn main() {
-    let target = if cfg!(feature = "static_link") {
-        println!("Building static bindings");
-        build_static()
+    // We ignore changes to the C library because it is always changed by the build process.
+    println!("cargo:rerun-if-changed=src");
+
+    let include_folder_path = if cfg!(feature = "dynamic_link") {
+        build_and_link_dynamic()
     } else {
-        println!("Building dynamic bindings");
-        link_dynamic()
+        build_and_link_static()
     };
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let builder = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("wrapper.h");
-
-    let builder = if let Some(target) = target {
-        builder.clang_args(&[format!("-I{}/include", target.to_string_lossy())])
-    } else {
-        builder
-    };
-
-    let bindings = builder
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    generate_bindings(&include_folder_path, "wrapper.h");
 }
